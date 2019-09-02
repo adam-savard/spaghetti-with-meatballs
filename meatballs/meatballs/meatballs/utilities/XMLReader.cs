94884e24b95b43c1d1298da9ddd3d191b23fcddd
adam-savard <savardfs@gmail.com>
2019-09-02 19:52:09 -0300

    #3: Converted to WFP because XAML isn't fun.

---
+++ meatballs/meatballs/meatballs/utilities/XMLReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace meatballs.utilities
+{
+    public static class XMLReader
+    {
+        //There is a bare minimum of 3 XML files that need to be parsed
+        //1. authors.xml
+        //2. files.xml
+        //3. functions.xml
+        //
+        //These files need to exist on disk. If they don't, they should be created.
+
+
+        static string path = AppDomain.CurrentDomain.BaseDirectory; //gets the path of the directory
+
+        public static string Path { get => path; }
+    }
+}
